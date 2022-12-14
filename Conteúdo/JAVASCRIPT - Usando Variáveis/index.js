81feedabdd166135f9be321f1de352c5d5097c14
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2022-12-14 11:00:23 -0300

    JAVASCRIPT - Usando Variáveis

---
+++ Conteúdo/JAVASCRIPT - Usando Variáveis/index.js
@@ -0,0 +1,23 @@
+console.log("Eu funciono!");
+console.log("Arquivo de script externo");
+
+// let meuNumero = 19;
+// let outroNumero = 15;
+
+// console.log(meuNumero + outroNumero);
+
+// console.log(` Meu número é: ${meuNumero + outroNumero}`);
+
+
+// let meuNumero = 19;
+// console.log(meuNumero);
+// aqui acontece a retribuição
+// meuNumero = 20;
+// console.log(meuNumero);
+
+// const meuNumero = 19;
+// console.log(meuNumero);
+// meuNumero = 20;
+// console.log(meuNumero);
+
+// var, let, const
