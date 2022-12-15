67bbe729d18e9567af7ad388865092a795ddebcf
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2022-12-15 20:50:40 -0300

    JAVASCRIPT - Condicionais e operadores lógicos

---
+++ Conteúdo/JAVASCRIPT - Condicionais e operadores lógicos/BOOLEANOS/index.js
@@ -0,0 +1,25 @@
+// let x = 5;
+// let y = 3;
+// let z = "5";
+
+// let idadeDoJoao = 22;
+// let idadeDaMaria = 15;
+
+// let joaoPodeEntrar = idadeDoJoao > 18;
+// console.log(joaoPodeEntrar);
+
+// let mariaPodeEntrar = idadeDaMaria > 18;
+// console.log(mariaPodeEntrar);
+
+// let x = 5;
+// let y = 3;
+
+//FALSY
+// false
+// undefined
+// null
+// ""
+// 0
+// NaN
+
+// let stringVazia = "";
