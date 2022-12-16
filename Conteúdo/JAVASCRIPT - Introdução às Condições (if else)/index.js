f6f69268423f2d4059ffc9a72ffc7baa389e3583
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2022-12-15 21:07:33 -0300

    JAVASCRIPT - Introdução às Condições (if/else)

---
+++ Conteúdo/JAVASCRIPT - Introdução às Condições (if else)/index.js
@@ -0,0 +1,27 @@
+// if (condição) {
+    // faça tal coisa
+// }
+
+// let x = 3;
+
+// if (x < 4) {
+//     console.log("X é menor que 4");
+// } 
+
+// let x = 3;
+// x = 5;
+// if (x < 4) {
+//     console.log("X é menor que 4");
+// } else {
+//     console.log("X não é menor que 4");
+// }
+
+// let x = 3;
+
+// if (x < 4) {
+//     console.log("X é menor que quatro");
+// } else if (x === 4) {
+//     console.log("X é igual a quatro");
+// } else {
+//     console.log("X é maior que quatro");
+// }
