3c0fd828c7185fcebcc51f9845f46423aba05486
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2022-12-16 19:38:24 -0300

    JAVASCRIPT - Arrays

---
+++ Conteúdo/JAVASCRIPT - Arrays/index.js
@@ -0,0 +1,34 @@
+//EXEMPLOS:
+// let numbers = [1, 2, 3];
+// let frutas  = ['banana', 'maçã', 'laranja', 'pêra'];
+// let numerosEFRutas = [1, 'banana', '3', 'maçã'];
+
+// console.log(numbers);
+// console.log(frutas);
+// console.log(numerosEFRutas);
+
+
+// let predio = ['terreo', 'primeiroAndar', 'segundoAndar', 'terceiroAndar',
+// 'quartoAndar'];
+
+// console.log(predio);
+// console.log(predio[3]);
+// console.log(predio[0]);
+// console.log(predio[5]);
+
+
+//Adiciona um item dentro da Array
+// predio.push('quintoAndar');
+// console.log(predio);
+
+//Elimina o útilmo item da Array
+// predio.pop();
+// console.log(predio);
+
+//Adiciona um item como primeiro item da Array
+// predio.unshift('subsolo');
+// console.log(predio);
+
+//Elimina o primeiro item da Array
+// predio.shift();
+// console.log(predio);
