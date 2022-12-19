7c2f7c258cbc4bc299eaf3b44f4c8f15e487f752
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2022-12-19 18:35:46 -0300

    JAVASCRIPT - Estruturas de repetição

---
+++ Conteúdo/JAVASCRIPT - Estruturas de repetição/index.js
@@ -0,0 +1,42 @@
+// for (inicial; condição; incremento) {
+//     // instruções
+// }
+
+// enquanto contador for menor que 3...
+// for (let contador = 0; contador < 3; contador++) {
+//     console.log("Kenzie");
+// }
+
+
+// houveram três iterações
+// ??? - contador é menor que três
+
+//1
+// contador = 0
+// contador < 3 ??? true
+// "Kenzie"
+
+//2
+// contador ++ =>
+// contador = 1
+// contador < 3 ??? true
+// "Kenzie"
+
+//3
+// contador ++ =>
+// contador = 1
+// contador < 3 ??? true
+// "Kenzie"
+
+//4
+// contador ++ =>
+// contador = 3
+// contador < 3 ??? false
+// PARA POR AQUI----
+
+// enquanto i for menor que o comprimento desse Array: 0, 1 e 2 será incrementado um valor
+// let states = ["São Paulo", "Rio de Janeiro", "Bahia"]
+
+// for (let i = 0; i < states.length; i++) {
+//     console.log(states[i])
+// }
