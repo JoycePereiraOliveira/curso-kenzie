e179e23a0e5cea1833fea08aa4f718f016ff2f9f
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2022-12-20 17:33:24 -0300

    JAVASCRIPT - Objetos em Javascript

---
+++ Conteúdo/JAVASCRIPT - Objetos em Javascript/index.js
@@ -0,0 +1,32 @@
+// let minhaXicara = {
+//     cor: "Marrom", // chave Cor e seu valor se tornam uma propriedade
+//     peso: 30,
+// }
+
+
+// let meuPrimeiroItemDoPedido = {
+//     id: 0,
+//     nome: "X-Burguer",
+//     preco: 30,
+//     disponivel: true,
+//     ingredientes: ["pão", "tomate", "alface", "hamburger", "queijo"],
+// };
+
+// console.log(meuPrimeiroItemDoPedido.ingredientes[0]);
+
+// console.log(meuPrimeiroItemDoPedido.preco);
+// meuPrimeiroItemDoPedido.preco = 25;
+// console.log(meuPrimeiroItemDoPedido.preco);
+
+// meuPrimeiroItemDoPedido.peso = "500g";
+// console.log(meuPrimeiroItemDoPedido);
+
+
+// meuPrimeiroItemDoPedido.peso = "500g";
+// console.log("o lanche " + meuPrimeiroItemDoPedido.nome + " é muito bom");
+
+
+// let nomeDoProduto = meuPrimeiroItemDoPedido.nome;
+
+// meuPrimeiroItemDoPedido.peso = "500g";
+// console.log("o lanche " + nomeDoProduto + " é muito bom");
