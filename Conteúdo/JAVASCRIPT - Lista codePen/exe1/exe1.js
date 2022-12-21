58ec80cc8207163763e4e355730b0f6bf59fda69
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2022-12-21 20:58:36 -0300

    JAVASCRIPT - Lista codePen

---
+++ Conteúdo/JAVASCRIPT - Lista codePen/exe1/exe1.js
@@ -0,0 +1,9 @@
+let nome; // Declare uma variável chamada 'nome'
+nome = "Joyce"; // Atribua um nome como valor da variável
+// Descomente a última linha
+
+let ____ = "Joyce" //"escreva um/seu nome aqui";
+
+alert('Olá, seja bem-vindo(a), ' + nome + '!')
+
+
