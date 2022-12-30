0b4017a154e66599d676228f4aa2dfb276771ac8
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2022-12-30 09:44:53 -0300

    Lista codePen - Oitavo Exercício

---
+++ Conteúdo/JAVASCRIPT - Lista codePen/exe8/index.js
@@ -0,0 +1,15 @@
+// Escreva um código que imprime o maior número entre dois números
+
+let numero1 = 7;
+let numero2 = 9;
+
+// if(numero1 _ numero2)
+if(numero1 > numero2){
+  // executa se a condição for verdadeira
+  // console.log(numero_);  
+  console.log(numero1);
+} else{
+  // executa se a condição for falsa
+  // console.log(numero_);
+  console.log(numero2);
+}
