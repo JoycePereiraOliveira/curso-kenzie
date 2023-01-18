24d6b0ed39d8c760796efa1db904a7b88517fd05
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2023-01-18 18:52:55 -0300

    Quarta Lista codePen - Terceiro Exercício

---
+++ Conteúdo/JAVASCRIPT - Lista codePen/Quarta Lista codePen/exe3/index.js
@@ -0,0 +1,14 @@
+// Você pode guardar diferentes tipos de dados
+// em uma mesma array, veja um exemplo abaixo
+
+// let random = ['exemplo', 55, true]
+
+// Agora crie uma variável 'pessoa'
+// nessa array, adicione nome(string), sobrenome(string)
+// e idade(inteiro)
+
+// let ______ = [ _, _, _ ]; // Adicione os valores aqui
+let pessoa = [ 'nome', 'sobrenome', 'idade' ];
+
+// Fique a vontade para fazer alguns testes
+// adicionar mais valores, criar mais arrays
