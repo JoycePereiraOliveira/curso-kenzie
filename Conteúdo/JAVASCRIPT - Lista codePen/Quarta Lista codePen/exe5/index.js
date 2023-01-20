5fb14b68962eb1c5fec98e1d94a0d57b75c3e9f0
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2023-01-20 17:33:26 -0300

    Quarta Lista codePen - Quinto Exercício

---
+++ Conteúdo/JAVASCRIPT - Lista codePen/Quarta Lista codePen/exe5/index.js
@@ -0,0 +1,24 @@
+// Loop for
+// Vamos refazer o exercício anterior
+// mas dessa vez, utilizando o loop for
+
+// *Obs: cuidado com os loops infinitos
+
+// let frutas = ['Melancia', 'Laranja', 'Maçã', 'Mamão', 'Morango', 'Manga', 'Kiwi', 'Bergamota', 'Tomate'];
+
+// for(let i = 0; i < frutas.length; i++){
+//   console.log(frutas[i]);
+// }
+
+// **Obs: o .length retorna o tamanho/quantidade de
+// elementos de uma array
+
+let linguagens = ['HTML', 'CSS', 'Javascript', 'Python', 'Java', 'PHP', 'Go'];
+
+for(let i = 0; i < linguagens.length; i++){
+  console.log(linguagens[i]);
+}
+// Utilize o exemplo de loop acima para
+// imprimir as linguagens no console
+
+// Dica: Não copie e cole, escreva! Vai por mim!
