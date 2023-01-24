a00ec5aac4c561cccbf3c86493eb52db6923227b
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2023-01-23 21:31:45 -0300

    Quarta Lista codePen - Sexto Exercício

---
+++ Conteúdo/JAVASCRIPT - Lista codePen/Quarta Lista codePen/exe6/index.js
@@ -0,0 +1,25 @@
+// Loop while
+// *Obs: cuidado com os loops infinitos
+
+// let frutas = ['Melancia', 'Laranja', 'Maçã', 'Mamão', 'Morango', 'Manga', 'Kiwi', 'Bergamota', 'Tomate'];
+
+// let i = 0;
+// while(i < frutas.length){
+//   console.log(frutas[i]);
+//   i++;
+// }
+
+// **Obs: o .length retorna o tamanho/quantidade de
+// elementos de uma array
+
+let linguagens = ['HTML', 'CSS', 'Javascript', 'Python', 'Java', 'PHP', 'Go'];
+
+let i = 0;
+while(i < linguagens.length){
+  console.log(linguagens[i]);
+  i++;
+}
+// Utilize o exemplo de loop acima para
+// imprimir as linguagens no console
+
+// Dica: Não copie e cole, escreva! Vai por mim!
