f7926a4e7433830d8d7970afa3ab3e3655dd6d46
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2023-02-09 22:17:19 -0300

    Funções Lista codePen - Primeiro Exercício

---
+++ Conteúdo/JAVASCRIPT - Funções lista codePen/exercícios/exe1/index.js
@@ -0,0 +1,7 @@
+// Crie uma função chamada areOlder que recebe a idade de uma pessoa e retorna um boolean (true ou false) se a pessoa é de maior ou não.
+
+function areOlder(age){
+    return age >= 18;
+  }
+  
+  console.log(areOlder(18));
