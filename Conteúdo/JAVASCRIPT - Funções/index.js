df56698d3f1c1c9bb6ae663ee2bdb76efcfab8e2
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2023-02-15 21:05:47 -0300

    JAVASCRIPT - Funções

---
+++ Conteúdo/JAVASCRIPT - Funções/index.js
@@ -0,0 +1,31 @@
+// function nomeDaFuncao(parametro1, parametro2){
+
+// }
+
+function somaNumeros(a, b){
+    let resultadoSoma = a + b;
+    return resultadoSoma;
+}
+
+function subtraiNumeros(a, b){
+    let resultadoSubtracao = a - b;
+    return resultadoSubtracao;
+}
+
+// console.log(resultado, resultado2);
+let resultado = somaNumeros(5, 8)
+let resultado2 = somaNumeros(2, 3)
+console.log(resultado);
+console.log(resultado2);
+
+// não irei utilizar as variáveis
+// subtraiNumeros(5, 2);
+// subtraiNumeros(9, 5);
+console.log(subtraiNumeros(5, 2));
+console.log(subtraiNumeros(9, 5));
+
+function digaOla(){
+    alert("Olá");
+}
+
+digaOla();
